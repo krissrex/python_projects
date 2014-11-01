1e2d54ecb2c679dcf8ac4e11602398b892b03d68
Kristian Rekstad <krirek@msn.com>
2014-11-01 22:05:48 +0100

    Refactoring, added deletion and debug argument.
    * Prepended _ to a lot of functions in NodeGrapher, as they were private. 
    * Removed getter from Node.
    * Added the ability to delete links with CTRL+Right Click.
    * Added the ability to delete all links with Delete. If there are no links, all nodes will be deleted.
    * Added the ability to test if "debug" is in the arguments when running to program, and then a bunch of default nodes and links will be created.

--- Projects/nodeGraph.py
+++ Projects/nodeGraph.py
@@ -6,7 +6,7 @@ Created on Wed Aug 27 22:23:08 2014
 """
 
 import tkinter
-import math
+from sys import argv
 
 
 class Link(object):
@@ -25,31 +25,12 @@ class Link(object):
             self.end = node
             node.links.append(self)
 
-    def getStartNode(self):
-        if isinstance(self.start, Node):
-            return self.start
-        else:
-            return None
-
-    def getEndNode(self):
-        if isinstance(self.end, Node):
-            return self.end
-        else:
-            return None
-
 
 class Node(object):
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
         self.links = []
-
-    def setXY(self, x, y):
-        self.x = x
-        self.y = y
-
-    def getLinks(self):
-        return self.links
 
 
 class NodeGrapher(object):
@@ -58,147 +39,208 @@ class NodeGrapher(object):
         self.E = self.getValidElementsList(E, Link)
         self.selectedLinkStart = None
         self.selectedLinkEnd = None
-        self.initGUI()
+        self.selectedNode = None
+        self._initGUI()
 
     def show(self):
         tkinter.mainloop()
 
     def getValidElementsList(self, elementList, className):
-        if isinstance(elementList, list):
-            tempList = []
-            for i in elementList:
-                if isinstance(i, className):
-                    tempList.append(i)
-            return tempList
+        return [element for element in elementList if isinstance(element, className)]
 
-    def initGUI(self):
+    def _initGUI(self):
         self.main = tkinter.Tk()
         self.main.title("Node Grapher")
-        self.main.bind("<Button-1>", self.clicked)
-        self.main.bind("<B1-Motion>", self.dragging)
-        self.main.bind("<ButtonRelease-1>", self.released)
+        self.main.bind("<Button-1>", self._clicked)
+        self.main.bind("<B1-Motion>", self._dragging)
+        self.main.bind("<ButtonRelease-1>", self._released)
+        self.main.bind("<Delete>", self._delete_all)
 
-        #For adding nodes
-        self.main.bind("<Control-Button-1>", self.ctrlClicked)
+        # For adding nodes
+        self.main.bind("<Control-Button-1>", self._ctrlClicked)
 
-        #add a canvas
+        # Add a canvas to draw points and lines
         self.canvas = tkinter.Canvas(self.main, width=680, height=500)
-        #For rightclicking a node
-        self.canvas.tag_bind("node", "<Button-3>", self.nodeRightClicked)
-
+        # For rightclicking a node
+        self.canvas.tag_bind("node", "<Button-3>", self._nodeRightClicked)
+        self.canvas.tag_bind("node", "<Control-Button-3>", self._nodeCtrlRightClicked)
         self.canvas.pack()
 
-        #Draw nodes
-        self.render()
+        # Draw nodes
+        self._render()
 
-        #Show the window (enter mainloop)
-        #self.show()
-
-    def ctrlClicked(self, event):
+    def _ctrlClicked(self, event):
         x = self.canvas.canvasx(event.x)
         y = self.canvas.canvasy(event.y)
         node = Node(x, y)
         self.V.append(node)
-        self.drawNode(node)
+        self._drawNode(node)
 
-    def nodeRightClicked(self, event):
-        x = self.canvas.canvasx(event.x)
-        y = self.canvas.canvasy(event.y)
+    def _nodeRightClicked(self, event):
+        x, y = self._canvas_coords(event)
+        node = self._getClosestNode(x, y)
 
-        node = self.getClosestNode(x, y)
-
-        if not (self.selectedLinkStart):
+        # Select the node and color its point, if one has not been selected already
+        if not self.selectedLinkStart:
             self.selectedLinkStart = node
-            self.canvas.itemconfig(node.p, tags=("node", "selected"),
-                                   fill="red")
-        elif self.selectedLinkStart and (node != self.selectedLinkStart):
-            self.selectedLinkEnd = node
-            self._connectNodes()
+            self.canvas.itemconfig(node.p, tags=("node", "selected"), fill="red")
+        else:
+            # If the same node was selected twice, reset it.
+            # Otherwise, the node was the end node.
+            if node == self.selectedLinkStart:
+                self.canvas.itemconfig(self.selectedLinkStart.p, tags="node", fill="cyan")
+                self.selectedLinkStart = None
+            else:
+                self.selectedLinkEnd = node
+                self._connectNodes()
+
+    def _nodeCtrlRightClicked(self, event):
+        x, y = self._canvas_coords(event)
+        node = self._getClosestNode(x, y)
+
+        if not self.selectedLinkStart:
+            self.selectedLinkStart = node
+            self.canvas.itemconfig(node.p, tags=("node", "selected"), fill="red")
+        else:
+            # Use [:] to make a copy of the list, to avoid breaking iteration
+            for link in self.selectedLinkStart.links[:]:
+                if node == link.end or node == link.start:
+                    try:
+                        link.end.links.remove(link)
+                    except Exception:
+                        pass
+                    try:
+                        link.start.links.remove(link)
+                    except Exception:
+                        pass
+                    try:
+                        self.E.remove(link)
+                    except Exception:
+                        pass
+                    self.canvas.delete(link.l)
+
+
+                
+
+    def _delete_all(self, event):
+        """Delete all lines and links. If there are none, delete all nodes and points."""
+        if self.E:
+            self.E = []
+            self.canvas.delete("link")
+        elif self.V:
+            self.V = []
+            self.canvas.delete("node")
+
+    def _canvas_coords(self, event):
+        return (self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))
 
     def _connectNodes(self):
-        if isinstance(self.selectedLinkStart, Node) and \
-                isinstance(self.selectedLinkEnd, Node):
+        """Link the nodes assigned as selected in the NodeGrapher and draw them."""
+        if isinstance(self.selectedLinkStart, Node) \
+                and isinstance(self.selectedLinkEnd, Node):
+            # Create a Link with the two selected nodes
             link = Link(self.selectedLinkStart, self.selectedLinkEnd)
             self.selectedLinkStart.links.append(link)
             self.selectedLinkEnd.links.append(link)
             self.E.append(link)
-            self.drawLink(link)
-            self.sendLinksBack()
+            self._drawLink(link)
+            self._sendLinksBack()
             self.canvas.itemconfig(self.selectedLinkStart.p, tags="node",
                                    fill="cyan")
+        # Clear the selection.
         self.selectedLinkStart = None
         self.selectedLinkEnd = None
 
-    def clicked(self, event):
-        x = self.canvas.canvasx(event.x)
-        y = self.canvas.canvasy(event.y)
-        self.selectedNode = self.getClosestNode(x, y)
+    def _clicked(self, event):
+        x, y = self._canvas_coords(event)
+        self.selectedNode = self._getClosestNode(x, y)
 
-    def dragging(self, event):
+    def _dragging(self, event):
         node = self.selectedNode
         if node:
-            node.setXY(event.x, event.y)
+            node.x = event.x
+            node.y = event.y
+            # update the drawn point with id in node.p and radius 5
             self.canvas.coords(node.p, int(node.x-5), int(node.y-5),
                                int(node.x+5), int(node.y+5))
-            for link in node.getLinks():
-                s = link.getStartNode()
-                e = link.getEndNode()
+            for link in node.links:
+                s = link.start
+                e = link.end
+                # update the drawn line with id in link.l
+                # and start in s.x, s.y and end e.x, e.y
                 self.canvas.coords(link.l, s.x, s.y, e.x, e.y)
 
-    def released(self, event):
+    def _released(self, event):
         self.selectedNode = None
 
-    def render(self):
+    def _render(self):
         self.canvas.create_rectangle(0, 0, 680, 500, fill="green")
 
         for point in self.V:
-            self.drawNode(point)
+            self._drawNode(point)
         for line in self.E:
-            self.drawLink(line)
-        self.sendLinksBack()
+            self._drawLink(line)
+        self._sendLinksBack()
 
-    def drawNode(self, node):
+    def _drawNode(self, node):
         points = [node.x-5, node.y-5, node.x+5, node.y+5]
         node.p = self.canvas.create_oval(points, fill="cyan", tags="node")
 
-    def drawLink(self, link):
-        start = link.getStartNode()
-        end = link.getEndNode()
+    def _drawLink(self, link):
+        start = link.start
+        end = link.end
         points = [start.x, start.y, end.x, end.y]
+        # Create a line in the canvas and assign its id to link.l
         link.l = self.canvas.create_line(points, tags="link")
 
-    def sendLinksBack(self):
-        self.canvas.tag_lower("link", "node")
+    def _sendLinksBack(self):
+        """Sends all lines in the canvas innwards so they are behind the points."""
+        # Lower all elements with tag 'link' below those with tag 'node'
+        try:
+            self.canvas.tag_lower("link", "node")
+        except:
+            print("Failed to send back lines. Perhaps there are none?")
 
-    def getClosestNode(self, x, y):
-        #Initial distance and value
-        dist = math.sqrt((self.V[0].x-x)**2+(self.V[0].y-y)**2)
-        closestNode = self.V[0]
+    def _getClosestNode(self, x, y):
+        if not self.V:
+            # No nodes in the list of vertices.
+            return
+
+        def distance_squared(node):
+            # No need to find the square root, as it is just comparison.
+            return (node.x - x)**2 + (node.y - y)**2
+
+        # Initial distance and value
+        dist = distance_squared(self.V[0])
+        closest_node = self.V[0]
 
         #Iterate over nodes and find closest
         for node in self.V:
-            nodeDist = math.sqrt((node.x-x)**2+(node.y-y)**2)
-            if nodeDist <= dist:
-                closestNode = node
-                dist = nodeDist
-        return closestNode
+            current_dist = distance_squared(node)
+            if current_dist <= dist:
+                closest_node = node
+                dist = current_dist
+        return closest_node
 
 
-def main():
+def main(args):
+
     nodes = []
     edges = []
-    for x in range(0, 500, 50):
-        nodes.append(Node(0.5*x+x % 100, x+25))
+    if "debug" in args:
+        for x in range(0, 500, 50):
+            nodes.append(Node(0.5*x+x % 100, x+25))
 
-    c = 0
-    while (c < len(nodes)-1):
-        edges.append(Link(nodes[c], nodes[c+1]))
-        if c == 3:
-            edges.append(Link(nodes[c], nodes[c+2]))
-        c += 1
+        c = 0
+        while (c < len(nodes)-1):
+            edges.append(Link(nodes[c], nodes[c+1]))
+            if c == 3:
+                edges.append(Link(nodes[c], nodes[c+2]))
+            c += 1
+        
 
     window = NodeGrapher(nodes, edges)
     window.show()
 
 if __name__ == "__main__":
-    main()
+    main(argv)
